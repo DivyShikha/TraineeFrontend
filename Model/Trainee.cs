2b2e961c8da7e3bd893d14bfcfd4a70028cca612
DivyShikha <divyshikha1890@gmail.com>
2025-08-26 16:32:50 +0530

    Add project files.

---
+++ Model/Trainee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForecast.Model
+{
+    public class Trainee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        public String Email { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        [Required]
+        public String Phone { get; set; }
+    }
+
+}
---
+++ Model/TraineeContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeatherForecast.Model
+{
+    public class TraineeContext : DbContext
+    {
+        public TraineeContext(DbContextOptions<TraineeContext> options) : base(options)
+        { 
+        }
+        public DbSet<Trainee> Trainees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
